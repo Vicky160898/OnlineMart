109368019a5970fbad55bdd4f2f475bba6c7e922
vinayak hiwase <vinayakhiwse8@gmail.com>
2023-03-05 19:22:04 +0530

    f

--- FrontEnd/mart/src/Redux/order/action.js
+++ FrontEnd/mart/src/Redux/order/action.js
@@ -8,9 +8,11 @@ import {
 export const PlaceOrder = (OrderId, user, toast) => async (dispatch) => {
   dispatch({ type: CREATE_ODER_REQUEST });
   try {
-    let res = await axios.get(`http://localhost:8080/api/orders/${OrderId}`, {
-      headers: { authorization: `Bearer ${user.token}` },
-    });
+    let res = await axios.get(`http://localhost:8080/api/orders/${OrderId}`
+    // , {
+    //   headers: { authorization: `Bearer ${user.token}` },
+    // }
+    );
     dispatch({ type: CREATE_ODER_SUCCESS, payload: res.data });
   } catch (e) {
     dispatch({ type: CREATE_ODER_FAILED, payload: e.message });
